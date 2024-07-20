44acb6787379710a91099cee91db2f30844a75db
anasxonbahodirov <anasxonbahodirov1@gmail.com>
2024-07-20 12:08:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let a = +prompt('Hafta sonini kiriting')
+
+switch(a){
+    case 1:
+        alert("Dushanba");
+        break;
+    case 2:
+        alert("Seshanba");
+        break;
+    case 3:
+        alert("Chorshanba");
+        break;
+    case 4:
+        alert("Payshanba");
+        break;
+    case 5:
+        alert("Juma");
+        break;
+    case 6:
+        alert("Shanba");
+        break;
+    case 7:
+        alert("Yakshanba");
+        break;
+    default:
+        alert("Haftada 7 kun bor");
+}
